19b96560cdf5be3b3c6dda308df9d7c879dd4174
darioestupinan <dario.estupinan@gmail.com>
2018-08-21 21:32:10 -0500

    adding moment and using location and timezone api

--- server/service.js
+++ server/service.js
@@ -4,18 +4,51 @@ const express = require('express');
 const service = express();
 const request = require('superagent');
 const config = require('../config');
+const moment = require('moment');
 
-service.get('/service/:location', (req, res, next) => {
-
-    request.get('https://maps.googleapis.com/maps/api/geocode/json')
+function getLocation(req, res) {
+    const comMapsApiGeocodeJson = 'https://maps.googleapis.com/maps/api/geocode/json';
+     return request
+        .get(comMapsApiGeocodeJson)
         .query({ address: req.params.location, key: config.geocodingKey})
         .end((err, response) => {
             if (err) {
                 console.log(err);
                 return res.sendStatus(500);
             }
-            res.json(response.body.results[0].geometry.location);
+
+            return getTime({value: response.body.results[0].geometry.location}, res);
         });
+}
+
+function getTime(location, res) {
+    const timestamp = +moment().format('X');
+    const url = 'https://maps.googleapis.com/maps/api/timezone/json';
+    const latlng = `${location.value.lat},${location.value.lng}`;
+
+    request.get(url)
+        .query({
+            location: latlng,
+            timestamp: timestamp,
+            key: config.geocodingKey
+        })
+        .end((err, response) => {
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+
+            const result = response.body;
+            const timeString = moment
+                .unix(timestamp + result.dstOffset + result.rawOffset)
+                .utc()
+                .format('dddd, MMMM Do YYYY, h:mm:ss a');
+            return {value: res.json({result: timeString}) };
+        });
+}
+
+service.get('/service/:location', (req, res, next) => {
+    getLocation(req, res);
 });
 
 module.exports = service;
